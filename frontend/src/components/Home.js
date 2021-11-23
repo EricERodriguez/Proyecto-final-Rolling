7fb40afcd1c15139d9624cb1ffcf83cbb7ff4997
Eric R <eric92rodriguez@gmail.com>
2021-11-22 22:45:04 -0300

    se borraron comentarios

--- frontend/src/components/Home.js
+++ frontend/src/components/Home.js
@@ -4,8 +4,6 @@ import HomeNotLogin from './Home/HomeNotLogin';
 // import Dashboard from "./Dashboard";
 import Cards from './HomeLogin/Cards';
 import Carrousel from './HomeLogin/Carrousel';
-import Login from "./LoginSystem/Login";
-import Register from "./LoginSystem/Register";
 
 class Home extends Component {
   render() {
@@ -20,16 +18,9 @@ class Home extends Component {
               <hr/>
               <Cards/>
             </div>
-            
           ) :
           <div>
             <div className="row">
-              {/* <div className="col-md">
-                <Login auth={this.props.auth} />
-              </div>
-              <div className="col-md">
-                <Register />
-              </div>, */}
               <HomeNotLogin/>
             </div>
           </div>
